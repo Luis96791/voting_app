c5e7b0f068e2d5161f813a5d040028e953aa390e
Luis96791 <luis.fajardozap@hotmail.com>
2018-03-09 23:15:24 -0600

    agregados app-1 y app-2 los errores de consola solucionados

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,13 @@
 class ProductList extends React.Component {
   render() {
     return (
-      <div className='ui unstackable items'>
-        Hello, friend! I am a basic React component.
-      </div>
+     	<Product />
     );
   }
 }
 
 ReactDOM.render(
-  <ProductList />,
-  document.getElementById('content')
+	<ProductList />,
+	document.getElementById('content')
 );
 
